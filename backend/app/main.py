0ea3dc13b2d10a6df596495cbcfcb252c7f57f39
Adya Pandey <adya07pandey@gmail.com>
2026-04-03 15:42:45 +0530

    Debug

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,31 +27,33 @@
 # def root():
 #     return {"message": "running"}
 
-try:
-    print("🚀 Starting import phase...")
 
-    from fastapi import FastAPI
-    print("✅ FastAPI imported")
+print("🚀 Starting import phase...")
 
-    from app.database.db import engine
-    print("✅ DB imported")
+from fastapi import FastAPI
+print("✅ FastAPI imported")
 
-    from app.database.models import Base
-    print("✅ Models imported")
+from app.database.db import engine
+print("✅ DB imported")
 
-    from fastapi.middleware.cors import CORSMiddleware
-    from app.api.auth_routes import router as auth_router
-    from app.api.email_routes import router as email_router
-    from app.api.logs_routes import router as logs_router
-    from app.api.google_routes import router as google_router
-    from app.api.dashboard_routes import router as dashboard_router
+from app.database.models import Base
+print("✅ Models imported")
+
+from fastapi.middleware.cors import CORSMiddleware
+print("✅ cors imported")
+from app.api.auth_routes import router as auth_router
+print("✅ router imported")
+    # from app.api.email_routes import router as email_router
+    # from app.api.logs_routes import router as logs_router
+    # from app.api.google_routes import router as google_router
+    # from app.api.dashboard_routes import router as dashboard_router
 
 #     from dotenv import load_dotenv
 #     import os
 
 #     load_dotenv()
 
-    app = FastAPI(title="FlowMind AI")
+app = FastAPI(title="FlowMind AI")
 
 #     # app.add_middleware(
 #     #     CORSMiddleware,
@@ -61,9 +63,9 @@ try:
 #     #     allow_headers=["*"],
 #     # )
 
-    print("🚀 Creating DB...")
-    Base.metadata.create_all(bind=engine)
-    print("✅ Database connected")
+# print("🚀 Creating DB...")
+# Base.metadata.create_all(bind=engine)
+# print("✅ Database connected")
 
 #     # app.include_router(auth_router)
 #     # app.include_router(google_router)
@@ -71,14 +73,14 @@ try:
 #     # app.include_router(logs_router)
 #     # app.include_router(dashboard_router)
 
-    @app.get("/")
-    def root():
-        return {"message": "FlowMind AI running"}
+@app.get("/")
+def root():
+    return {"message": "FlowMind AI running"}
 
-except Exception as e:
-    import traceback
-    print("❌ FULL ERROR:")
-    traceback.print_exc()
+# except Exception as e:
+#     import traceback
+#     print("❌ FULL ERROR:")
+#     traceback.print_exc()
 
 # # uvicorn app.main:app --host localhost --port 8000 --reload 
 # # curl -X POST https://dominik-imprescriptible-kimberlee.ngrok-free.dev/emails/gmail/webhook ^
